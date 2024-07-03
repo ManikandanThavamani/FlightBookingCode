56b0cfb93cc3993caa20b62d4fef84794cfc2539
mthavamani <mthavamani@192.168.0.103>
2024-07-03 12:18:03 +0530

    Master

---
+++ src/test/java/com/javatpoint/LoginAndRegistrationPage/LoginAndRegistrationPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatpoint.LoginAndRegistrationPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegistrationPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
